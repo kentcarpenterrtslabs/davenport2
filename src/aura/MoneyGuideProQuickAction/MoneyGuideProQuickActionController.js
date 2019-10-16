e93ce6f18ad183502696ca4ef4f720e36897704e
Kent Carpenter <kent.carpenter@rtslabs.com>
2019-10-16 15:09:15 -0400

    Made MGP quick action correctly retrieve and display login as user picklist. Made MGP qucik action redirect to SF connected app URL to kick off SAML response. Modified queries used by MGP to get the list of login as users to use the Rep Security object instead of the deprecated Rep Assignment object.

--- src/aura/MoneyGuideProQuickAction/MoneyGuideProQuickActionController.js
+++ src/aura/MoneyGuideProQuickAction/MoneyGuideProQuickActionController.js
@@ -3,17 +3,30 @@
  */
 ({
     doInit: function(component, event, helper){
-        var action = component.get("c.buildRequestBody");
+
+        var action = component.get("c.getPicklistOptions");
         action.setParam('householdId', component.get("v.recordId"));
-        action.setCallback(this, function(response){
+
+        action.setCallback(this, function(response) {
             var state = response.getState();
-            if (state == 'SUCCESS'){
-                var returnValue = response.getReturnValue();
-                component.set("v.request", returnValue);
-                component.set("v.requestDisplay", helper.formatXml(returnValue));
-            }else{
-                console.log('buildRequestBody ' + state);
-                console.log(response.getError());
+
+            if (state == 'SUCCESS') {
+                var users = response.getReturnValue();
+				console.log('getPicklistOptions users' + users);
+				var loginAsOptions = [];
+                users.forEach(function(user) {
+                    loginAsOptions.push({
+                        "label" : user.FirstName + ' ' + user.LastName,
+                        "value" : user.Id
+                    });
+                });
+				component.set("v.loginAsOptions", loginAsOptions);
+                //component.set("v.requestDisplay", helper.formatXml(returnValue));
+
+            } else {
+
+                console.log('getPicklistOptions ' + state);
+                console.log('getPicklistOptions ' + response.getError());
             }
         });
         $A.enqueueAction(action);
@@ -21,18 +34,22 @@
 
     submit: function(component, event, helper){
         var updateUserAction = component.get("c.updateUser");
+
         updateUserAction.setParam('householdId', component.get("v.recordId"));
-        updateUserAction.setParam('userId', null); // todo: pass from picklist once we have values
+        updateUserAction.setParam('userId', component.find("loginAsCmbx").get("v.value"));
+
         updateUserAction.setCallback(this, function(response){
             var state = response.getState();
-            if (state == 'SUCCESS'){
+
+            if (state == 'SUCCESS') {
                 var eUrl= $A.get("e.force:navigateToURL");
                 eUrl.setParams({
-                  "url": 'https://qa.moneyguidepro.com/davenport/ssosamlnoref.aspx?IntegrationId=170'
+				  "url" : 'https://investdavenport--rtskent.my.salesforce.com/idp/login?app=0sp0v0000004CON'
+				  //"url": 'https://investdavenport--rtskent.my.salesforce.com/idp/login?app=0sp0b000000XZVT'
                 });
                 eUrl.fire();
-            }else{
-                console.log('buildRequestBody ' + state);
+            } else {
+                //console.log('buildRequestBody ' + state);
                 console.log(response.getError());
             }
         });
@@ -40,3 +57,4 @@
         $A.enqueueAction(updateUserAction);
     }
 })
+//"url": 'https://qa.moneyguidepro.com/davenport/ssosamlnoref.aspx?IntegrationId=170'
