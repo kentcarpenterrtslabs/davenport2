1bbf54984a92ffbf280e52cb85169bde9c365e81
Kent Carpenter <kent.carpenter@rtslabs.com>
2019-10-17 14:51:14 -0400

    Added fields to User object to store MGP first name, last name and username of the chosen "login as" MGP user. Added code to MGP component and controller to use and populate those fields. Added custom metadata object to store configuration for MGP (such as the Idp URL). Added code to MGP component and controller to retrieve and use the custom metadata. Moved code form MGP controller that belongs in the MGP Household classs. Updated tests for all MGP classes.

--- src/aura/MoneyGuideProQuickAction/MoneyGuideProQuickActionController.js
+++ src/aura/MoneyGuideProQuickAction/MoneyGuideProQuickActionController.js
@@ -33,28 +33,36 @@
     },
 
     submit: function(component, event, helper){
-        var updateUserAction = component.get("c.updateUser");
+		var selectedLoginAsUser = component.find("loginAsCmbx").get("v.value");
 
-        updateUserAction.setParam('householdId', component.get("v.recordId"));
-        updateUserAction.setParam('userId', component.find("loginAsCmbx").get("v.value"));
+		if (typeof selectedLoginAsUser === "undefined") {
+			var error = {};
+			error.message = "Please choose a user.";
+			component.find("errorMessage").setError(error);
+		} else {
+			var updateUserAction = component.get("c.updateUser");
 
-        updateUserAction.setCallback(this, function(response){
-            var state = response.getState();
+			updateUserAction.setParam('householdId', component.get("v.recordId"));
+			updateUserAction.setParam('userId', selectedLoginAsUser);
 
-            if (state == 'SUCCESS') {
-                var eUrl= $A.get("e.force:navigateToURL");
-                eUrl.setParams({
-				  "url" : 'https://investdavenport--rtskent.my.salesforce.com/idp/login?app=0sp0v0000004CON'
-				  //"url": 'https://investdavenport--rtskent.my.salesforce.com/idp/login?app=0sp0b000000XZVT'
-                });
-                eUrl.fire();
-            } else {
-                //console.log('buildRequestBody ' + state);
-                console.log(response.getError());
-            }
-        });
+			updateUserAction.setCallback(this, function(response){
+				var state = response.getState();
 
-        $A.enqueueAction(updateUserAction);
+				if (state == 'SUCCESS') {
+					var eUrl= $A.get("e.force:navigateToURL");
+
+					var idpURL = response.getReturnValue();
+					eUrl.setParams({
+					  "url" : idpURL
+					});
+					eUrl.fire();
+				} else {
+					//console.log('buildRequestBody ' + state);
+					console.log(response.getError());
+				}
+			});
+
+			$A.enqueueAction(updateUserAction);
+		}
     }
 })
-//"url": 'https://qa.moneyguidepro.com/davenport/ssosamlnoref.aspx?IntegrationId=170'
