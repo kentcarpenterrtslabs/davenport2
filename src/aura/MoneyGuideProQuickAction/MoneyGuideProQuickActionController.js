036e85501a81fca3e630b09976867385c99ff943
Kent Carpenter <kent.carpenter@rtslabs.com>
2019-11-07 12:50:50 -0500

    For the MGP compenent, user with an Advisor license may only login to MGP as themselves. Compliance and Admin licensed users may login as any MGP licensed user hat has access to the household.

--- src/aura/MoneyGuideProQuickAction/MoneyGuideProQuickActionController.js
+++ src/aura/MoneyGuideProQuickAction/MoneyGuideProQuickActionController.js
@@ -6,29 +6,33 @@
 
         var action = component.get("c.getHouseholdData");
         action.setParam('householdId', component.get("v.recordId"));
+		action.setParam('loggedInUserId', $A.get("$SObjectType.CurrentUser.Id"));
 
         action.setCallback(this, function(response) {
             var state = response.getState();
 
             if (state == 'SUCCESS') {
                 var householdInfo = response.getReturnValue();
-				console.log('getPicklistOptions users' + householdInfo.mgpUsers);
-				var loginAsOptions = [];
-                householdInfo.mgpUsers.forEach(function(user) {
-                    loginAsOptions.push({
-                        "label" : user.FirstName + ' ' + user.LastName,
-                        "value" : user.Id
-                    });
-                });
-				component.set("v.loginAsOptions", loginAsOptions);
 
-				if (! householdInfo.hasPrimaryContact) {
+				if (! householdInfo.hasMGPLicense) {
+					component.set("v.cannotOpenMGPTitle","No MGP License");
+					component.set("v.cannotOpenMGPReason","You must have a license to connect to Money Guide Pro.");
+				} else if (! householdInfo.hasPrimaryContact) {
 					component.set("v.cannotOpenMGPTitle","No Primary Contact On This Account");
 					component.set("v.cannotOpenMGPReason","A household must have a primary contact to use Money Guide Pro.");
 				} else if (householdInfo.mgpUsers.length <= 0) {
 					component.set("v.cannotOpenMGPTitle","No Money Guide Pro Users Can Access This Account");
 					component.set("v.cannotOpenMGPReason","There are no users that have access to this household and have access to Money Guide Pro.");
 				} else {
+					console.log('getPicklistOptions users' + householdInfo.mgpUsers);
+					var loginAsOptions = [];
+					householdInfo.mgpUsers.forEach(function(user) {
+						loginAsOptions.push({
+							"label" : user.FirstName + ' ' + user.LastName,
+							"value" : user.Id
+						});
+					});
+					component.set("v.loginAsOptions", loginAsOptions);
 					component.set("v.allowOpenMGP", true);
 				}
             } else {
